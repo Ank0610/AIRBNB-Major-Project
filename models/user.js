5945f27e67fe315d98f959c9d4b7e411f98441fe
ANKIT RAJ <beingankitkumar7272@gmail.com>
2024-12-16 18:39:15 +0530

    Your commit message

--- models/user.js
+++ models/user.js
@@ -2,7 +2,8 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 const passportLocalMongoose = require("passport-local-mongoose");
 
-const userSchema = Schema({
+// const userSchema = Schema({
+const userSchema = new mongoose.Schema({
     email:{
         type: String,
         required: true
